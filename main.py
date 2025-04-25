3aa48295f1098f8928f3341e24caf646fe76a3b5
Cag1710 <cag1710@outlook.com>
2025-04-25 16:51:07 +1000

    Added correlation heatmap and average daily power usage graph

---
+++ main.py
@@ -0,0 +1,27 @@
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# reads the csv
+df = pd.read_csv('server_data.csv')
+
+# draws up a correlation heat map between power and some variables of the dataset
+corr_map = df[['power', 'voltage', 'current', 'ESP32_temp', 'WORKSTATION_CPU', 'WORKSTATION_GPU', 'WORKSTATION_RAM']].corr()
+sns.heatmap(corr_map, annot=True, cmap="coolwarm")
+plt.title("Feature correlation with Power usage")
+plt.show()
+
+# draws up a line graph that shows the average power usage of the servers each day
+df['server_date'] = pd.to_datetime(df['server_date'])
+df['date'] = df['server_date'].dt.date
+daily_avg_power = df.groupby('date')['power'].mean()
+plt.figure(figsize=(12, 5))
+daily_avg_power.plot()
+plt.xlabel('Time')
+plt.ylabel('Power (w)')
+plt.title('Average Daily Power Usage')
+plt.grid(True)
+plt.tight_layout()
+plt.show()
+
