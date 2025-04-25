41737abed900097cd7cc80776699b08921086f0b
Cag1710 <cag1710@outlook.com>
2025-04-25 16:57:41 +1000

    Made the graphs functions so we can call them if need be

--- main.py
+++ main.py
@@ -7,21 +7,26 @@ import pandas as pd
 df = pd.read_csv('server_data.csv')
 
 # draws up a correlation heat map between power and some variables of the dataset
-corr_map = df[['power', 'voltage', 'current', 'ESP32_temp', 'WORKSTATION_CPU', 'WORKSTATION_GPU', 'WORKSTATION_RAM']].corr()
-sns.heatmap(corr_map, annot=True, cmap="coolwarm")
-plt.title("Feature correlation with Power usage")
-plt.show()
+def corr_heat_map():
+    corr_map = df[['power', 'voltage', 'current', 'ESP32_temp', 'WORKSTATION_CPU', 'WORKSTATION_GPU', 'WORKSTATION_RAM']].corr()
+    sns.heatmap(corr_map, annot=True, cmap="coolwarm")
+    plt.title("Feature correlation with Power usage")
+    plt.show()
 
 # draws up a line graph that shows the average power usage of the servers each day
-df['server_date'] = pd.to_datetime(df['server_date'])
-df['date'] = df['server_date'].dt.date
-daily_avg_power = df.groupby('date')['power'].mean()
-plt.figure(figsize=(12, 5))
-daily_avg_power.plot()
-plt.xlabel('Time')
-plt.ylabel('Power (w)')
-plt.title('Average Daily Power Usage')
-plt.grid(True)
-plt.tight_layout()
-plt.show()
+def average_daily_power_graph():
+    df['server_date'] = pd.to_datetime(df['server_date'])
+    df['date'] = df['server_date'].dt.date
+    daily_avg_power = df.groupby('date')['power'].mean()
+    plt.figure(figsize=(12, 5))
+    daily_avg_power.plot()
+    plt.xlabel('Time')
+    plt.ylabel('Power (w)')
+    plt.title('Average Daily Power Usage')
+    plt.grid(True)
+    plt.tight_layout()
+    plt.show()
+
+corr_heat_map()
+average_daily_power_graph()
 
