e1335b84bc71c0590293b964bf0a93e7367f1dd9
Cag1710 <cag1710@outlook.com>
2025-04-30 09:31:03 +1000

    changed the csv, dates past 09-19 were erratic and missing, also changed main.py to have an energy usage graph and not power graph as energy should be the target variable ive realised

--- main.py
+++ main.py
@@ -4,29 +4,37 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # reads the csv
-df = pd.read_csv('server_data.csv')
+df = pd.read_csv('filtered_data.csv')
 
 # draws up a correlation heat map between power and some variables of the dataset
 def corr_heat_map():
-    corr_map = df[['power', 'voltage', 'current', 'ESP32_temp', 'WORKSTATION_CPU', 'WORKSTATION_GPU', 'WORKSTATION_RAM']].corr()
+    corr_map = df[["power", "energy", "voltage","current","ESP32_temp","WORKSTATION_CPU","WORKSTATION_GPU","WORKSTATION_RAM"]].corr()
     sns.heatmap(corr_map, annot=True, cmap="coolwarm")
     plt.title("Feature correlation with Power usage")
     plt.show()
 
-# draws up a line graph that shows the average power usage of the servers each day
-def average_daily_power_graph():
+# shows the total energy used for a given day in kWh (making this the target variable now, realised its better suited)
+def daily_energy_usage(df):
     df['server_date'] = pd.to_datetime(df['server_date'])
     df['date'] = df['server_date'].dt.date
-    daily_avg_power = df.groupby('date')['power'].mean()
+    df = df.sort_values('server_date')
+
+    # Get the last energy reading each day
+    daily_totals = df.groupby('date')['energy'].last()
+
+    # Find the daily consumption by differencing
+    daily_consumed = daily_totals.diff()
+
+    # Plot
     plt.figure(figsize=(12, 5))
-    daily_avg_power.plot()
-    plt.xlabel('Time')
-    plt.ylabel('Power (w)')
-    plt.title('Average Daily Power Usage')
+    daily_consumed.plot()
+    plt.title("Daily Energy Consumption (kWh)")
+    plt.xlabel("Date")
+    plt.ylabel("Energy Used (kWh)")
     plt.grid(True)
     plt.tight_layout()
     plt.show()
 
-corr_heat_map()
-average_daily_power_graph()
 
+corr_heat_map()
+daily_energy_usage(df)
